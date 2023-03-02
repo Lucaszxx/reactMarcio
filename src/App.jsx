5efacdd223f74937c4d21c9c9af2d0fb65329502
EA - Lucas Da Silva Gomes <silva_gomes01@educadventista.org>
2023-03-01 22:50:58 -0300

    Subindo o projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Results } from "./components/Results";
+import { Summary } from "./components/Summary";
+
+export function App() {
+    return (
+        <main>
+            <Results/>
+            <Summary/>
+        </main>
+    )
+}
